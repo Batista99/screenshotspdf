685b5a8b773106bed57c895b11126ef6c2b58184
Pedro Batista <pedrobatista89@gmail.com>
2023-07-16 01:28:42 +0200

    update hook

--- captureScreenshotsServer.js
+++ captureScreenshotsServer.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('public')); // Serve the HTML file
 
-app.post('https://api.netlify.com/build_hooks/64b327bf29229957db067a13', (req, res) => {
+app.post('/execute-script', (req, res) => {
   try {
     const scriptProcess = spawn('node', ['captureScreenshotsPDF-urlsPRDPreviewOrLiveL0L1v2.js']);
 
@@ -27,6 +27,26 @@ app.post('https://api.netlify.com/build_hooks/64b327bf29229957db067a13', (req, r
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.get('/trigger-build', (req, res) => {
+  // Send an HTTP POST request to the Netlify build hook URL
+  const { spawn } = require('child_process');
+  const curlProcess = spawn('curl', ['-X', 'POST', 'https://api.netlify.com/build_hooks/64b327bf29229957db067a13']);
+
+  curlProcess.stdout.on('data', (data) => {
+    console.log(`Build hook response: ${data}`);
+  });
+
+  curlProcess.stderr.on('data', (data) => {
+    console.error(`Build hook error: ${data}`);
+  });
+
+  curlProcess.on('close', (code) => {
+    console.log(`Build hook process exited with code ${code}`);
+    res.sendStatus(200);
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
