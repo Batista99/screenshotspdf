cf592424c23a20ae71207ca9d0673c6b139375f0
Pedro Batista <pedrobatista89@gmail.com>
2023-07-16 02:05:33 +0200

    logs

--- functions/execute-script.js
+++ functions/execute-script.js
@@ -3,9 +3,7 @@ const { spawn } = require('child_process');
 
 exports.handler = function(event, context, callback) {
   try {
-    console.log('Starting captureScreenshotsPDF-urlsPRDPreviewOrLiveL0L1v2.js script execution');
-
-    const scriptPath = path.join(process.cwd(), 'captureScreenshotsPDF-urlsPRDPreviewOrLiveL0L1v2.js');
+    const scriptPath = path.join(__dirname, '..', 'captureScreenshotsPDF-urlsPRDPreviewOrLiveL0L1v2.js');
     console.log('Script path:', scriptPath);
 
     const scriptProcess = spawn('node', [scriptPath]);
