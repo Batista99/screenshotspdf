78d757f0d60f349291c0e78815f31b06dd30d53c
Pedro Batista <pedrobatista89@gmail.com>
2023-07-16 02:21:09 +0200

    logs

--- functions/execute-script.js
+++ functions/execute-script.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const { spawn } = require('child_process');
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context) {
   try {
     const scriptPath = path.resolve(__dirname, '..', '..', 'captureScreenshotsPDF-urlsPRDPreviewOrLiveL0L1v2.js');
     console.log('Script path:', scriptPath);
 
     const scriptProcess = spawn('node', [scriptPath], {
-      cwd: '/var/task',
+      cwd: process.env.LAMBDA_TASK_ROOT,
       shell: true,
     });
 
@@ -19,26 +19,28 @@ exports.handler = function(event, context, callback) {
       console.error(`Script error: ${data}`);
     });
 
-    scriptProcess.on('close', (code) => {
-      console.log(`Script process exited with code ${code}`);
-      callback(null, {
-        statusCode: 200,
-        body: 'Script execution completed',
+    return new Promise((resolve, reject) => {
+      scriptProcess.on('close', (code) => {
+        console.log(`Script process exited with code ${code}`);
+        resolve({
+          statusCode: 200,
+          body: 'Script execution completed',
+        });
       });
-    });
 
-    scriptProcess.on('error', (error) => {
-      console.error('Script process error:', error);
-      callback(null, {
-        statusCode: 500,
-        body: 'Error executing script',
+      scriptProcess.on('error', (error) => {
+        console.error('Script process error:', error);
+        reject({
+          statusCode: 500,
+          body: 'Error executing script',
+        });
       });
     });
   } catch (error) {
     console.error('Error executing script:', error);
-    callback(null, {
+    return {
       statusCode: 500,
       body: 'Error executing script',
-    });
+    };
   }
 };
